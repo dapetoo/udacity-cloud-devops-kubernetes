04dbd285663644eea98e194754e599607584283f
Peter Dada <dapetoo@gmail.com>
2022-07-03 08:55:50 +0100

    Kubernetes shell commands

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,11 +9,16 @@ dockerpath = dapetoo/flaskapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run flaskapp \
+    --image=$dockerpath \
+    --image-pull-policy="Always"
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward flaskapp 8080:80
 
